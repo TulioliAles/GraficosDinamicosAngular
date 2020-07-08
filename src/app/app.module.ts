ac5250c48614568f02e735f6c3f5c243bcaf60f3
TulioliAles <desmecad@hotmail.com>
2020-07-08 20:31:04 -0300

    Commit 2 - Criação de Gráficos Barras, Donuts, Pizza, Polar, Radar e Dinâmico(Barras + Linhas)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ChartsModule } from 'ng2-charts';
 import { LinhasComponent } from './components/linhas/linhas.component';
+import { BarrasComponent } from './components/barras/barras.component';
+import { DonutsComponent } from './components/donuts/donuts.component';
+import { RadarComponent } from './components/radar/radar.component';
+import { PizzaComponent } from './components/pizza/pizza.component';
+import { PolarComponent } from './components/polar/polar.component';
+import { DinamicoComponent } from './components/dinamico/dinamico.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LinhasComponent
+    LinhasComponent,
+    BarrasComponent,
+    DonutsComponent,
+    RadarComponent,
+    PizzaComponent,
+    PolarComponent,
+    DinamicoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/donuts/donuts.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Label, MultiDataSet } from 'ng2-charts';
+import { ChartType } from 'chart.js';
+
+@Component({
+  selector: 'app-donuts',
+  templateUrl: './donuts.component.html',
+  styleUrls: ['./donuts.component.css']
+})
+export class DonutsComponent implements OnInit {
+
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+    [50, 150, 120],
+    [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+}
